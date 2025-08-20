15c8be0dab65eb3a88ea618ef0a623058c5bc216
Muskan06-star <shrivastavamuskan45@gmail.com>
2025-08-20 19:51:25 +0530

    base url

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: 'https://task-manager-full-stack-app-n0xi.onrender.com/api',
 });
 export default api;
